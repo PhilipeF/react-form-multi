097de7b22bc61baa3590d121c5a0193b0f21f3b5
rick <philipesferreiraa@gmail.com>
2022-12-07 11:25:37 -0300

    adicionando o arquivo gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import { GrFormPrevious, GrFormNext } from 'react-icons/gr';
+
 import './index.css';
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>Testando</h1>
+    <div className="app">
+      <div className="header">
+        <h2>Deixe a sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <p>etapas</p>
+        <form>
+          <div className="input-container">
+          </div>
+          <button type='button'>
+            <GrFormPrevious />
+            <span>Voltar</span>
+          </button>
+          <button type='submit'>
+            <span>Avançar</span>
+            <GrFormNext />
+          </button>
+        </form>
+
+      </div>
     </div>
   )
 }
